45ce649d9bb1bd0bc9766fd92dfceeb9dd147db3
blogscot <iain.diamond@gmail.com>
2015-07-10 12:27:29 +0100

    Use HTML5 routing

--- app/js/app.js
+++ app/js/app.js
@@ -1,24 +1,26 @@
 'use strict';
 
 var eventsApp = angular.module('eventsApp', ['ngResource', 'ngRoute'])
-  .config(function($routeProvider) {
+  .config(function($routeProvider, $locationProvider) {
     $routeProvider.when('/newEvent',
     {
-      templateUrl: 'templates/NewEvent.html',
+      templateUrl: '/templates/NewEvent.html',
       controller: 'EditEventController'
     });
 
     $routeProvider.when('/events',
       {
-        templateUrl: 'templates/EventList.html',
+        templateUrl: '/templates/EventList.html',
         controller: 'EventListController'        
       });
 
     $routeProvider.when('/event/:eventId',
       {
-        templateUrl: 'templates/EventDetails.html',
+        templateUrl: '/templates/EventDetails.html',
         controller: 'EventController'        
       });
 
-    $routeProvider.otherwise({redirectTo: '/events'});
+    // $routeProvider.otherwise({redirectTo: '/events'});
+
+    $locationProvider.html5Mode(true);
   });
