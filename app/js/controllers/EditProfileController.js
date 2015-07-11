ab20603e48bb84bc38a815e0bbfb7824c716bb5e
blogscot <iain.diamond@gmail.com>
2015-07-11 12:47:44 +0100

    Add gravatar directive

--- app/js/controllers/EditProfileController.js
+++ app/js/controllers/EditProfileController.js
@@ -1,19 +1,11 @@
 'use strict';
-/*global eventsApp,md5Hasher*/
+/*global eventsApp,window,alert*/
 
 eventsApp.controller('EditProfileController',
-    function EditProfileController($scope, md5Hasher) {
+    function EditProfileController($scope, gravatarUrlBuilder) {
       $scope.user = {};
 
       $scope.getGravatarUrl = function(email) {
-        var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
-        var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        
-        if(!regex.test(email))
-          return defaultGravatarUrl;
-
-        var MD5 = md5Hasher.hash;
-
-        return 'http://www.gravatar.com/avatar/' + MD5(email) + ".jpg?s=200&r=x";
+        gravatarUrlBuilder.buildGravatarUrl(email);
       };
 });
--- app/js/services/GravatarUrlBuilder.js
+++ app/js/services/GravatarUrlBuilder.js
@@ -2,17 +2,17 @@
 /*globals eventsApp*/
 
 eventsApp.factory('gravatarUrlBuilder', function (md5Hasher) {
-    return {
-        buildGravatarUrl: function (email) {
-            var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
+  return {
+    buildGravatarUrl: function (email) {
+      var defaultGravatarUrl = "http://www.gravatar.com/avatar/000?s=200";
 
-            var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if(!regex.test(email))
-                return defaultGravatarUrl;
+      var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(!regex.test(email))
+        return defaultGravatarUrl;
 
-            var MD5 = md5Hasher.hash;
+      var MD5 = md5Hasher.hash;
 
-            return 'http://www.gravatar.com/avatar/' + MD5(email) + ".jpg?s=200&r=x";
-        }
-    };
+      return 'http://www.gravatar.com/avatar/' + MD5(email) + ".jpg?s=200&r=x";
+    }
+  };
 });
