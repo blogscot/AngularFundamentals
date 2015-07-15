82651ef44e62734cf707868d2bf2ae83448ed912
blogscot <iain.diamond@gmail.com>
2015-07-15 19:04:43 +0100

    Edit Profile bugfix

--- app/js/directives/gravatar.js
+++ app/js/directives/gravatar.js
@@ -4,14 +4,14 @@
 eventsApp.directive('gravatar', function (gravatarUrlBuilder) {
   return {
     scope: {
-      email: '='
+      email: '@'
     },
     restrict: 'E',
     template: "<img>",
     replace: true,
     link: function(scope, element, attrs, controller) {
       attrs.$observe('email', function(newValue, oldValue) {
-        if (newValue != oldValue) {
+        if (newValue !== oldValue) {
           attrs.$set('src', gravatarUrlBuilder.buildGravatarUrl(newValue));
         }
       });
